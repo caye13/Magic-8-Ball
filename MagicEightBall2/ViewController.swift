382f3ad072e5c128edd8047562957670cfbcfab0
caye13 <Caye1300@gmail.com>
2020-07-28 15:35:15 -0700

    Initial Commit

---
+++ MagicEightBall2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall2
+//
+//  Created by Ck2 Jedi on 7/28/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
