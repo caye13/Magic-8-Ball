277f6d79b9a0362b0535e2b089e1df8d9357a596
caye13 <Caye1300@gmail.com>
2020-07-28 20:11:14 -0700

    add shake

--- MagicEightBall2/ViewController.swift
+++ MagicEightBall2/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
 
+
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+       generateAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
 
+       generateAnswer()
+    }
+    
+    func generateAnswer() {
+        let randomIndex = Int.random(in: 0..<answers.count)
+        
+        answerLabel.text = answers[randomIndex]
+    }
 }
 
